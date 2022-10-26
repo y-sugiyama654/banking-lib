f3db70a2c932c43352a920d4e9da75efe5f64fd2
Yuta <y.sugiyama54@gmail.com>
2022-10-26 23:50:48 +0900

    extracted the error and logger from banking api

---
+++ errs/errors.go
@@ -0,0 +1,35 @@
+package errs
+
+import "net/http"
+
+type AppError struct {
+	Code    int    `json:",omitempty"`
+	Message string `json:"message"`
+}
+
+func NewNotFoundError(message string) *AppError {
+	return &AppError{
+		Message: message,
+		Code:    http.StatusNotFound,
+	}
+}
+
+func (e AppError) AsMessage() *AppError {
+	return &AppError{
+		Message: e.Message,
+	}
+}
+
+func NewUnexpectedError(message string) *AppError {
+	return &AppError{
+		Message: message,
+		Code:    http.StatusInternalServerError,
+	}
+}
+
+func NewValidationError(message string) *AppError {
+	return &AppError{
+		Message: message,
+		Code:    http.StatusUnprocessableEntity,
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module banking-lib
+
+go 1.19
